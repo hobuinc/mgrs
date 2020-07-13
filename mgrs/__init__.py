795d639a64bdc3d17245687ae8abb4bba66c4217
Howard Butler <howard@hobu.co>
2020-07-13 12:51:38 -0500

    make flake8 nanny happy

--- mgrs/__init__.py
+++ mgrs/__init__.py
@@ -1,21 +1,20 @@
-from . core import rt
+from . import core
 
 import ctypes
-import re
 
-__version__='1.3.6'
+__version__ = '1.3.6'
+
 
 class MGRS:
     def __init__(self):
         pass
 
-
     def ddtodms(self, dd):
         """Take in dd string and convert to dms"""
         negative = dd < 0
         dd = abs(dd)
-        minutes,seconds = divmod(dd*3600,60)
-        degrees,minutes = divmod(minutes,60)
+        minutes, seconds = divmod(dd * 3600, 60)
+        degrees, minutes = divmod(minutes, 60)
         if negative:
             if degrees > 0:
                 degrees = -degrees
@@ -23,11 +22,10 @@ class MGRS:
                 minutes = -minutes
             else:
                 seconds = -seconds
-        return (degrees,minutes,seconds)
+        return (degrees, minutes, seconds)
 
     def dmstodd(self, dms):
         """ convert dms to dd"""
-        size = len(dms)
         letters = 'WENS'
         is_annotated = False
 
@@ -39,7 +37,8 @@ class MGRS:
                     is_annotated = True
                     break
             if not is_annotated:
-                raise core.RTreeError("unable to parse '%s' to decimal degrees" % dms)
+                msg = "unable to parse '%s' to decimal degrees" % dms
+                raise core.RTreeError(msg)
         is_negative = False
         if is_annotated:
             dms_upper = dms.upper()
@@ -51,7 +50,7 @@ class MGRS:
 
         if is_annotated:
             bletters = letters.encode(encoding='utf-8')
-            bdms = dms.encode(encoding = 'utf-8')
+            bdms = dms.encode(encoding='utf-8')
             dms = bdms.translate(None, bletters).decode('ascii')
 
             # bletters = bytes(letters, encoding='utf-8')
@@ -70,7 +69,7 @@ class MGRS:
             M = dms[-4:-2]
             D = dms[:-4]
         else:
-            S = '{0:s}.{1:s}'.format (pieces[0][-2:], pieces[1])
+            S = '{0:s}.{1:s}'.format(pieces[0][-2:], pieces[1])
             M = pieces[0][-4:-2]
             D = pieces[0][:-4]
 
@@ -88,14 +87,14 @@ class MGRS:
             lon = longitude
 
         p = ctypes.create_string_buffer(80)
-        core.rt.Convert_Geodetic_To_MGRS( lat, lon, MGRSPrecision, p)
+        core.rt.Convert_Geodetic_To_MGRS(lat, lon, MGRSPrecision, p)
         return ctypes.string_at(p)
 
     def toLatLon(self, MGRS, inDegrees=True):
         plat = ctypes.pointer(ctypes.c_double())
         plon = ctypes.pointer(ctypes.c_double())
         c = ctypes.string_at(MGRS)
-        core.rt.Convert_MGRS_To_Geodetic( c, plat, plon)
+        core.rt.Convert_MGRS_To_Geodetic(c, plat, plon)
         if inDegrees:
             lat = core.TO_DEGREES(plat.contents.value)
             lon = core.TO_DEGREES(plon.contents.value)
@@ -104,20 +103,27 @@ class MGRS:
             lon = plon.contents.value
         return (lat, lon)
 
-    def MGRSToUTM (self, MGRS) :
-        mgrs       = ctypes.string_at(MGRS)
-        zone       = ctypes.pointer(ctypes.c_long())
+    def MGRSToUTM(self, MGRS):
+        mgrs = ctypes.string_at(MGRS)
+        zone = ctypes.pointer(ctypes.c_long())
         hemisphere = ctypes.pointer(ctypes.c_char())
-        easting    = ctypes.pointer(ctypes.c_double())
-        northing   = ctypes.pointer(ctypes.c_double())
+        easting = ctypes.pointer(ctypes.c_double())
+        northing = ctypes.pointer(ctypes.c_double())
 
         core.rt.Convert_MGRS_To_UTM(mgrs, zone, hemisphere, easting, northing)
 
-        return zone.contents.value, hemisphere.contents.value, easting.contents.value, northing.contents.value
+        return zone.contents.value,
+        hemisphere.contents.value,
+        easting.contents.value,
+        northing.contents.value
 
-    def UTMToMGRS (self, zone, hemisphere, easting, northing, MGRSPrecision=5) :
+    def UTMToMGRS(self, zone, hemisphere, easting, northing, MGRSPrecision=5):
         mgrs = ctypes.create_string_buffer(80)
 
-        core.rt.Convert_UTM_To_MGRS(zone, ctypes.c_char(hemisphere), ctypes.c_double(easting), ctypes.c_double(northing), MGRSPrecision, mgrs)
+        core.rt.Convert_UTM_To_MGRS(zone,
+                                    ctypes.c_char(hemisphere),
+                                    ctypes.c_double(easting),
+                                    ctypes.c_double(northing),
+                                    MGRSPrecision, mgrs)
 
         return mgrs.value
--- mgrs/core.py
+++ mgrs/core.py
@@ -1,8 +1,10 @@
-import atexit, os, re, sys
+import os
+import sys
 import ctypes
 from ctypes.util import find_library
 import sysconfig
 import math
+
 
 class RTreeError(Exception):
     "RTree exception, indicates a RTree-related error."
@@ -14,12 +16,13 @@ def get_windows_platform_name():
     try:
         import wheel.pep425tags
         name = wheel.pep425tags.get_abbr_impl() + \
-			   wheel.pep425tags.get_impl_ver() + \
-			   '-' + wheel.pep425tags.get_platform(None)
+            wheel.pep425tags.get_impl_ver() + \
+            '-' + wheel.pep425tags.get_platform(None)
         return libname + '.' + name + '.pyd'
     except ImportError:
         return libname + '.pyd'
-        
+
+
 if os.name == 'nt':
     try:
         local_dlls = sys.path
@@ -28,13 +31,18 @@ if os.name == 'nt':
         # Python
         platform = get_windows_platform_name()
         rt = ctypes.PyDLL(platform)
+
+        free = None
+
         def free(m):
+            global free
             try:
                 free = ctypes.cdll.msvcrt.free(m)
             except WindowsError:
                 pass
     except (ImportError, WindowsError):
         raise
+
 elif os.name == 'posix':
     platform = os.uname()[0]
     soabi = sysconfig.get_config_var('SOABI')
@@ -48,17 +56,18 @@ elif os.name == 'posix':
 else:
     raise RTreeError('Unsupported OS "%s"' % os.name)
 
-errors = {  0x0001: "Latitude Error",
-            0x0002: "Longitude Error",
-            0x0004: "String Error",
-            0x0008: "Precision Error",
-            0x0010: "Axis Error",
-            0x0020: "Inverse Flattening Error",
-            0x0040: "Easting Error",
-            0x0080: "Northing Error",
-            0x0100: "Zone Error",
-            0x0200: "Hemisphere Error"
-         }
+errors = {0x0001: "Latitude Error",
+          0x0002: "Longitude Error",
+          0x0004: "String Error",
+          0x0008: "Precision Error",
+          0x0010: "Axis Error",
+          0x0020: "Inverse Flattening Error",
+          0x0040: "Easting Error",
+          0x0080: "Northing Error",
+          0x0100: "Zone Error",
+          0x0200: "Hemisphere Error"
+          }
+
 
 def get_errors(value):
     output = 'MGRS Errors: '
@@ -67,8 +76,10 @@ def get_errors(value):
             output += errors[key] + " & "
     return output[:-2]
 
+
 def TO_RADIANS(degrees):
     return (float(degrees) * math.pi/180.0)
+
 
 def TO_DEGREES(radians):
     return (float(radians) * 180.0/math.pi)
@@ -77,7 +88,7 @@ def TO_DEGREES(radians):
 def check_error(result, func, cargs):
     "Error checking proper value returns"
     if result != 0:
-        msg = 'Error in "%s": %s' % (func.__name__, get_errors(result) )
+        msg = 'Error in "%s": %s' % (func.__name__, get_errors(result))
         raise RTreeError(msg)
     return
 
@@ -99,7 +110,10 @@ def check_error(result, func, cargs):
 #  *
 #  */
 
-rt.Convert_Geodetic_To_MGRS.argtypes = [ctypes.c_double, ctypes.c_double, ctypes.c_long, ctypes.c_char_p]
+rt.Convert_Geodetic_To_MGRS.argtypes = [ctypes.c_double,
+                                        ctypes.c_double,
+                                        ctypes.c_long,
+                                        ctypes.c_char_p]
 rt.Convert_Geodetic_To_MGRS.restype = ctypes.c_long
 rt.Convert_Geodetic_To_MGRS.errcheck = check_error
 
@@ -107,7 +121,7 @@ rt.Convert_Geodetic_To_MGRS.errcheck = check_error
 # /*
 #  * This function converts an MGRS coordinate string to Geodetic (latitude
 #  * and longitude in radians) coordinates.  If any errors occur, the error
-#  * code(s) are returned by the  function, otherwise MGRS_NO_ERROR is returned.
+#  * code(s) are returned by the  function, otherwise MGRS_NO_ERROR is returned
 #  *
 #  *    MGRS       : MGRS coordinate string           (input)
 #  *    Latitude   : Latitude in radians              (output)
@@ -116,7 +130,9 @@ rt.Convert_Geodetic_To_MGRS.errcheck = check_error
 #  */
 #
 
-rt.Convert_MGRS_To_Geodetic.argtypes = [ctypes.c_char_p, ctypes.POINTER(ctypes.c_double), ctypes.POINTER(ctypes.c_double)]
+rt.Convert_MGRS_To_Geodetic.argtypes = [ctypes.c_char_p,
+                                        ctypes.POINTER(ctypes.c_double),
+                                        ctypes.POINTER(ctypes.c_double)]
 rt.Convert_MGRS_To_Geodetic.restype = ctypes.c_long
 rt.Convert_MGRS_To_Geodetic.errcheck = check_error
 
@@ -135,7 +151,12 @@ rt.Convert_MGRS_To_Geodetic.errcheck = check_error
 #  *    MGRS       : MGRS coordinate string           (output)
 #  */
 
-rt.Convert_UTM_To_MGRS.argtype = [ctypes.c_long, ctypes.c_char, ctypes.c_double, ctypes.c_double, ctypes.c_long, ctypes.c_char_p]
+rt.Convert_UTM_To_MGRS.argtype = [ctypes.c_long,
+                                  ctypes.c_char,
+                                  ctypes.c_double,
+                                  ctypes.c_double,
+                                  ctypes.c_long,
+                                  ctypes.c_char_p]
 rt.Convert_UTM_To_MGRS.restype = ctypes.c_long
 rt.Convert_UTM_To_MGRS.errcheck = check_error
 
@@ -153,6 +174,10 @@ rt.Convert_UTM_To_MGRS.errcheck = check_error
 #  *    Northing   : Northing (Y) in meters           (output)
 #  */
 
-rt.Convert_MGRS_To_UTM.argtype = [ctypes.c_char_p, ctypes.POINTER(ctypes.c_long), ctypes.POINTER(ctypes.c_char), ctypes.POINTER(ctypes.c_double), ctypes.POINTER(ctypes.c_double)]
+rt.Convert_MGRS_To_UTM.argtype = [ctypes.c_char_p,
+                                  ctypes.POINTER(ctypes.c_long),
+                                  ctypes.POINTER(ctypes.c_char),
+                                  ctypes.POINTER(ctypes.c_double),
+                                  ctypes.POINTER(ctypes.c_double)]
 rt.Convert_MGRS_To_UTM.restype = ctypes.c_long
 rt.Convert_MGRS_To_UTM.errcheck = check_error
