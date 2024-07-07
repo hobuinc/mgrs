53f837785c4b86c41fa6903f40b8669426648d18
Howard Butler <hobu.inc@gmail.com>
2024-07-07 06:50:40 -0500

    Build deploy (#43)
    * streamline setup.py and use pyproject.toml for most build definition stuff
    
    * build and deploy stuff
    
    * satisfy black/flake8
    
    * satisfy black
    
    * remove references to unexisting packages
    
    * adjustments
    
    * install editable for testing
    
    * install editable for testing again
    
    * name adjustments

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-import codecs
-
 from setuptools import Extension, setup
 
 sources = [
@@ -14,44 +12,9 @@ sources = [
 mgrs = Extension(
     "libmgrs",
     sources=sources,
-    define_macros=None,
     include_dirs=["./libmgrs"],
-    libraries=None,
-    library_dirs=None,
 )
 
+setup_args = dict(ext_modules=[mgrs])
 
-with codecs.open("./README.rst", encoding="utf-8") as f:
-    readme_text = f.read()
-
-setup(
-    name="mgrs",
-    version="1.4.6",
-    description="MGRS coordinate conversion for Python",
-    license="MIT",
-    keywords="gis coordinate conversion",
-    author="Howard Butler",
-    author_email="howard@hobu.co",
-    maintainer="Howard Butler",
-    maintainer_email="howard@hobu.co",
-    url="https://github.com/hobu/mgrs",
-    long_description=readme_text,
-    long_description_content_type="text/x-rst",
-    ext_modules=[mgrs],
-    install_requires=["packaging"],
-    packages=["mgrs"],
-    test_suite="tests.test_suite",
-    zip_safe=False,
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Programming Language :: C",
-        "Programming Language :: C++",
-        "Programming Language :: Python",
-        "Topic :: Scientific/Engineering :: GIS",
-        "Topic :: Database",
-    ],
-)
+setup(**setup_args)
